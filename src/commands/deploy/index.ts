bf0f6ba7b664366baac5b196cd33b2dbf242fe83
pikapirate <157611276+pikapirate@users.noreply.github.com>
2024-10-05 10:41:32 -0400

    Add deploy command

---
+++ src/commands/deploy/index.ts
@@ -0,0 +1,109 @@
+import {Args, Command, Flags} from '@oclif/core'
+import {Blob} from 'node:buffer'
+import {FormData, fetch} from 'undici'
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+import {nodeFileTrace} from '@vercel/nft';
+import AdmnZip from 'adm-zip';
+import {safely} from "../../lib/utils.js";
+import {BaseCommand} from "../../baseCommand.js";
+
+export default class Deploy extends BaseCommand<typeof Deploy> {
+    static description = 'Build the application'
+
+    static flags = {
+        projectDir: Flags.string({description: 'Relative path project directory', required: true}),
+        projectId: Flags.integer({description: 'Project ID', required: true}),
+        serviceId: Flags.integer({description: 'Service ID', required: true}),
+        runtime: Flags.string({description: 'Runtime', required: true}),
+    }
+
+    async run(): Promise<void> {
+        const {args, flags, argv} = await this.parse(Deploy)
+
+        if (!this.tildaConfig.v1.identity) {
+            this.error({
+                name: 'NOT_LOGGED_IN',
+                message: 'You are not logged in'
+            })
+        }
+        if (!this.apiClient) {
+            this.error({
+                name: 'API_CLIENT_NOT_INITIALIZED',
+                message: 'API client not initialized'
+            })
+        }
+
+        const tildaBuildFilePath = path.resolve(flags.projectDir, '.tilda', 'package.zip');
+        // check if build file exists
+        const [errorWithCheckingBuildFileStats, buildFileStats] = await safely<Awaited<ReturnType<typeof fs.stat>>, {
+            code: 'ENOENT',
+            message: string
+        }>(fs.stat(tildaBuildFilePath));
+        if (errorWithCheckingBuildFileStats) {
+            if (errorWithCheckingBuildFileStats.code !== 'ENOENT') {
+                this.error(`Error checking build file: ${errorWithCheckingBuildFileStats.message}`);
+            }
+        } else if (!buildFileStats.isFile()) {
+            this.error(`Build file is not a valid file: ${tildaBuildFilePath}`);
+        }
+
+        if (!buildFileStats) {
+            this.error(`Build file does not exist: ${tildaBuildFilePath}`);
+        }
+
+        if (buildFileStats.size === 0) {
+            this.error(`Build file is empty: ${tildaBuildFilePath}`);
+        }
+
+        const [errorWithPackageUrl, packageUploadUrlResponse] = await safely(this.apiClient.getComputeServicePackageUploadUrl.mutate({
+            projectId: flags.projectId,
+            serviceId: flags.serviceId,
+            runtime: flags.runtime,
+            packageFileSizeBytes: 0,
+        }));
+        if (errorWithPackageUrl) {
+            this.error(`Error getting package upload URL: ${errorWithPackageUrl.message}`);
+        }
+
+        this.debug('Got package upload params', packageUploadUrlResponse);
+
+        const uploadData = new FormData();
+        for (const [key, value] of Object.entries(packageUploadUrlResponse.params)) {
+            uploadData.append(key, value);
+        }
+
+        const [errorWithReadingBuildFile, buildFileContents] = await safely(fs.readFile(tildaBuildFilePath));
+        if (errorWithReadingBuildFile) {
+            this.error(`Error reading build file: ${errorWithReadingBuildFile.message}`);
+        }
+
+        uploadData.append('file', new Blob([buildFileContents]));
+
+        this.debug('Uploading package to', packageUploadUrlResponse.endpointUrl);
+        const [uploadError, uploadResponse] = await safely(fetch(packageUploadUrlResponse.endpointUrl, {
+            method: 'POST',
+            body: uploadData,
+        }));
+        if (uploadError || !uploadResponse.ok) {
+            console.error(uploadError, uploadResponse);
+            this.error(`Error uploading package: ${uploadError?.message}`);
+        }
+        this.debug('Uploaded package', uploadResponse);
+
+        this.debug('Creating version');
+        const [errorWithCreatingVersion, versionResponse] = await safely(this.apiClient.createComputeServiceVersion.mutate({
+            projectId: flags.projectId,
+            serviceId: flags.serviceId,
+            uploadToken: packageUploadUrlResponse.uploadToken,
+            runtime: flags.runtime,
+        }));
+        if (errorWithCreatingVersion) {
+            this.error(`Error creating version: ${errorWithCreatingVersion.message}`);
+        }
+
+        this.debug('Created version', versionResponse);
+        const deployedHostnames = versionResponse?.routingConfig?.hostnames?.map((h: string) => `https://${h}/`)
+        this.log('Deployed version', deployedHostnames);
+    }
+}
