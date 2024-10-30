f0465a35fd142a4955843e4cbfa7816535fea9d9
pikapirate <157611276+pikapirate@users.noreply.github.com>
2024-10-30 18:34:43 -0400

    Fix cache handler

--- src/nextJsCacheHandler.cjs
+++ src/nextJsCacheHandler.cjs
@@ -1,26 +1,22 @@
 module.exports = class CacheHandler {
-    constructor(options) {
+    constructor(options, ...rest) {
         this.options = options;
 
         const globalCacheHandlers = globalThis[Symbol.for('@next/cache-handlers')];
         if (globalCacheHandlers?.FetchCache) {
-            // eslint-disable-next-line prefer-rest-params
-            this.globalCacheHandler = new globalCacheHandlers.FetchCache(arguments);
+            this.globalCacheHandler = new globalCacheHandlers.FetchCache(options, ...rest);
         }
     }
 
-    async get() {
-        // eslint-disable-next-line prefer-rest-params
-        return this.globalCacheHandler?.get(arguments);
+    async get(...params) {
+        return this.globalCacheHandler?.get(...params);
     }
 
-    async set() {
-        // eslint-disable-next-line prefer-rest-params
-        return this.globalCacheHandler?.set(arguments);
+    async set(...params) {
+        return this.globalCacheHandler?.set(...params);
     }
 
-    async revalidateTag() {
-        // eslint-disable-next-line prefer-rest-params
-        return this.globalCacheHandler?.revalidateTag(arguments);
+    async revalidateTag(...params) {
+        return this.globalCacheHandler?.revalidateTag(...params);
     }
 }
