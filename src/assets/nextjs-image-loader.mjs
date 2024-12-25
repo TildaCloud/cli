75b886f43454f7891c902c52f62b29869ff021ce
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2024-12-25 14:45:35 -0700

    Update Image Optimization path

--- src/assets/nextjs-image-loader.mjs
+++ src/assets/nextjs-image-loader.mjs
@@ -1,4 +1,4 @@
 export default function imageLoader({ src, width, quality }) {
     const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
-    return `/.well-known/tilda/image/${params.join(',')}/${src.toString().startsWith('/') ? src.slice(1) : src}`
+    return `/_tilda/image/${params.join(',')}/${src.toString().startsWith('/') ? src.slice(1) : src}`
 }
