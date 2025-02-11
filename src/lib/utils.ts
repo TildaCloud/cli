62da6b7f6d80b9c09461aaf111a2152300c27091
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2025-02-11 16:11:57 -0500

    Improve safely util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-export async function safely<T, E = Error>(fnOrPromise: (() => T) | Promise<T>): Promise<[E, undefined] | [undefined, T]> {
+export async function safely<T, E = Error>(fnOrPromise: (() => T) | Promise<T>): Promise<[E, undefined] | [undefined, Awaited<T>]> {
     try {
         const result: T = typeof fnOrPromise === 'function' ? fnOrPromise() : (await fnOrPromise);
-        return [undefined, result];
+        return [undefined, await result];
     } catch (error) {
         return [error as E, undefined];
     }
